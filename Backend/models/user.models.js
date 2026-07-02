95bd2cff166c534683e9f890d725e50833ebe5c5
Aishmal <aishmalshahzad234@gmail.com>
2026-07-03 00:43:56 +0500

    implemented

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -59,4 +61,12 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+userSchema.pre("save",async function (next) {
+  if(!this.modified("password"))return next()
+  this.password=bcrypt.hash(this.password,10)
+  next()
+})
+userSchema.methods.isPasswordCorrect=async function (password) {
+ return await bcrypt.compare(password,this.password)
+}
 export const User = mongoose.model("User", userSchema);
