1c97e7fb67076c75faf6b5404c4c6b147205a126
Aishmal <aishmalshahzad234@gmail.com>
2026-07-03 22:56:35 +0500

    add methods to generate tokens

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -10,6 +10,13 @@ const userSchema = new mongoose.Schema(
       trim:true, 
       index:true
     },
+    userName:{
+       type: String,
+      required: true,
+      lowercase:true,
+      trim:true, 
+      index:true
+    },
     secondName: {
       type: String, 
     },
@@ -68,5 +75,29 @@ userSchema.pre("save",async function (next) {
 })
 userSchema.methods.isPasswordCorrect=async function (password) {
  return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAccessToken=function(){
+   jwt.sign({
+    _id:this._id,
+    email:this.email,
+    userName :this.userName,
+    firstName:this .firstName
+
+   },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+   jwt.sign({
+    _id:this._id,
+   },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
 }
 export const User = mongoose.model("User", userSchema);
